c650ea6ac9efe6933479a2b5912a55abcf319f03
Carson Rey <c.rey494@gmail.com>
2018-09-17 21:54:50 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(string = "Hello there, and how are you?")
   string.split(" ")
-  string.each do |word|
+  string.collect do |word|
     word.reverse
   end
+  string.join(" ")
 end
